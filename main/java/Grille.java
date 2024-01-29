c3b9a589386acb6509cdc5bc6c3d79e323c1c379
DamiBoul <damien.boulanger2@gmail.com>
2024-01-29 11:33:50 +0100

    organisation + type enum pour les techniques + technique de depart

--- main/java/Grille.java
+++ main/java/Grille.java
@@ -1,6 +1,11 @@
 public class Grille {
 
     int c,l //nb colonnes et nb lignes
+
+    /**
+     * Parcourt la grille dans son état actuel pour vérifier les techniques qui s'appliquent aux îles et donner un indice au joueur.
+     * @return l'île qui peut bénéficier d'un indice.
+     */
     public Ile aide(){
         int fIndMin = Technique.TECHNIQUE_LIST.getLength(); //une liste des fonctions qui appliquent une technique
                                                             //elles prennent en paramètre une île, et renvoient vrai si la technique s'applique à l'île
@@ -11,7 +16,7 @@ public class Grille {
             for(int j,j<this.l,j++){
                 if(this.getIle(i,j) != null) {
                     for (int fInd, fInd<fInfMin, fInd++){
-                        if(Technique.TECHNIQUE_LIST.get(fInd).apply(this.getIle(i, j))){
+                        if(Technique.TECHNIQUE_LIST.get(fInd).apply(this.getIle(i, j))){ //si la technique s'applique à l'île
                             aideIle = this.getIle(i,j);
                             fInfMin = fInd; //on ne vérifie que les techniques de plus bas niveau que celles trouvées
                         }
---
+++ main/java/Technique.java
@@ -0,0 +1,51 @@
+import java.util.function.Function;
+
+/**
+ * Une technique correspond à une description et à un algoritmhe qui vérifie si
+ * une île peut bénéficier de la technique dans son état actuel (Ile -> Boolean).
+ */
+public enum Technique {
+    TECH_DEP("Si une île a un chiffre pair et son nombre de voisins correspond à n/2 alors elle est reliée 2 fois à chacun de ses voisins"
+            , o -> {
+
+        if(o.complete) return false; //si l'île est déjà complète, cette technique est inutile.
+
+        int n = o.getN();
+        if(n == 8) return true;
+
+        int x = o.getX();
+        int y = o.getY();
+        int l = o.getGrille().getL();
+        int c = o.getGrille().getC();
+        if(n == 6){
+            if(x == 0 || y == 0 || x == l-1 || y == c-1) return true;
+        }
+        if(n == 4){
+            if(x == 0){
+                if(y == 0) return true;
+                if(y == c-1) return true;
+            }
+            if(x == l-1){
+                if(y == 0) return true;
+                if(y == c-1) return true;
+            }
+        }
+        return false;
+    });
+
+    private final String description;
+    private final Function<Object, Boolean> fonction;
+
+    Technique(String description, Function<Object, Boolean> fonction) {
+        this.description = description;
+        this.fonction = fonction;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Function<Object, Boolean> getFonction() {
+        return fonction;
+    }
+}
