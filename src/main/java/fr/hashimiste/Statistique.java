d3aeadd49e7508dccc32bfecdb7683e33ff62835
Kylian Patault <123745029+KylianPatault@users.noreply.github.com>
2024-01-30 10:17:34 +0100

    Insertion d'une statistique dans la bdd

--- src/main/java/fr/hashimiste/Statistique.java
+++ src/main/java/fr/hashimiste/Statistique.java
@@ -6,28 +6,25 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Statistique {
-    public static void main(String[] args) {
-        String bddHashi = "base.db";
-
-        if (args.length != 1) {
-            System.out.println("Nombre d'argument incorrect : 1 arguments attendus");
-            System.out.println("Usage : java SaveStatistique \"valeur\"");
-            System.err.println("Nombre d'argument incorrect");
-        }
-
-        int val = Integer.valueOf(args[0]);
-
+     public String bddHashi = "base.db";
+    public void save(int val) {
         try {
-            Connection bdd = DriverManager.getConnection(bddHashi);
+            Class.forName("org.sqlite.JDBC");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:"+bddHashi);
 
             String insertQuery = "INSERT INTO statistique (id_stat, id_p, value) VALUES (?, ?, ?)";
-            PreparedStatement connect_stat = bdd.prepareStatement(insertQuery);
+            PreparedStatement connect_stat = connection.prepareStatement(insertQuery);
             connect_stat.setString(1, "DEFAULT");
             connect_stat.setString(2, "(SELECT id_profil FROM profil)");
             connect_stat.setInt(3, val);
             connect_stat.executeUpdate();
+
+            connect_stat.close();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
