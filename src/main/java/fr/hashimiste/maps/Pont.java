d056c49e562fab7f9881d54adfd14b03f332a401
enimaloc <antoine@enimaloc.fr>
2024-01-30 10:35:38 +0100

    Vérification des paramétre à la création des ponts

--- src/main/java/fr/hashimiste/maps/Pont.java
+++ src/main/java/fr/hashimiste/maps/Pont.java
@@ -15,6 +15,15 @@ public class Pont extends Component {
      * @param n    le nombre de ponts
      */
     public Pont(Ile ile1, Ile ile2, int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Le nombre de ponts ne peut pas être négatif");
+        }
+        if (ile1 == null || ile2 == null) {
+            throw new IllegalArgumentException("Les iles ne peuvent pas être nulles");
+        }
+        if (ile1.equals(ile2)) {
+            throw new IllegalArgumentException("Les iles ne peuvent pas être les mêmes");
+        }
         this.ile1 = ile1;
         this.ile2 = ile2;
         this.n = n;
