8816992d6c044fb4d6512454b5076b31ce1307eb
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2024-02-08 23:42:44 +0100

    Modification de couleur, image et menu.java par rapport au retour de la review
    Ajout de la doc.
    Respect des conventions de nommages.
    Allègement du constructeur de menu avec l'ajout de plusieurs méthodes.

--- src/main/java/fr/hashimiste/gui/Couleur.java
+++ src/main/java/fr/hashimiste/gui/Couleur.java
@@ -1,76 +1,33 @@
 package fr.hashimiste.gui;
-
+import java.awt.Color;
 /**
  * Classe pour les couleurs
  * @author Arthur Dureau
  */
 public class Couleur {
-    private static java.awt.Color couleurBouton = new java.awt.Color(160, 158, 188);
-    private static java.awt.Color couleurTextBouton = new java.awt.Color(251, 250, 242);
-    private static java.awt.Color couleurBoutonDesactive = new java.awt.Color(197, 179, 179);
-    private static java.awt.Color couleurFond = new java.awt.Color(251,250,242);
-    public final static java.awt.Color couleurTransparent=new java.awt.Color(0x00000000, true);
     /**
-     * Retourne la couleur du bouton
-     * @return Color
+     * Couleurs boutons
      */
-    public static java.awt.Color getCouleurBouton() {
-        return couleurBouton;
-    }
+    public final static Color COULEUR_BOUTON = new Color(160, 158, 188);
+    /**
+     * Couleurs texte boutons
+     */
+    public final static Color COULEUR_TEXTE_BOUTON = new Color(251, 250, 242);
+    /**
+     * Couleurs boutons désactivés
+     */
+    public final static Color COULEUR_BOUTON_DESACTIVE = new Color(197, 179, 179);
+    /**
+     * Couleurs fond
+     */
+    public final static Color COULEUR_FOND = new Color(251,250,242);
+    /**
+     * Couleurs transparent
+     */
+    public final static Color COULEUR_TRANSPARENT = new Color(0x00000000, true);
 
     /**
-     * Retourne la couleur du texte du bouton
-     * @return Color
-     */
-    public static java.awt.Color getCouleurTextBouton() {
-        return couleurTextBouton;
-    }
-
-    /**
-     * Retourne la couleur du bouton désactivé
-     * @return Color
-     */
-    public static java.awt.Color getCouleurBoutonDesactive() {
-        return couleurBoutonDesactive;
-    }
-
-    /**
-     * Retourne la couleur du fond
-     * @return Color
-     */
-    public static java.awt.Color getCouleurFond() {
-        return couleurFond;
-    }
-
-    /**
-     * Modifie la couleur du bouton
-     * @param couleurBouton
-     */
-    public void setCouleurBouton(java.awt.Color couleurBouton) {
-        Couleur.couleurBouton = couleurBouton;
-    }
-
-    /**
-     * Modifie la couleur du texte du bouton
-     * @param couleurTextBouton
-     */
-    public void setCouleurTextBouton(java.awt.Color couleurTextBouton) {
-        Couleur.couleurTextBouton = couleurTextBouton;
-    }
-
-    /**
-     * Modifie la couleur du bouton désactivé
-     * @param couleurBoutonDesactive
-     */
-    public void setCouleurBoutonDesactive(java.awt.Color couleurBoutonDesactive) {
-        Couleur.couleurBoutonDesactive = couleurBoutonDesactive;
-    }
-
-    /**
-     * Modifie la couleur du fond
-     * @param couleurFond
-     */
-    public void setCouleurFond(java.awt.Color couleurFond) {
-        Couleur.couleurFond = couleurFond;
-    }
+     * Constructeur privée pour éviter l'instanciation
+     **/
+    private Couleur() {}
 }
