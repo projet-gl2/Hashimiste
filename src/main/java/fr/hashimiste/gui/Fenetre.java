954645b56f59d96a082127b0d6b4c4070bc1f547
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2024-02-21 16:02:16 +0100

    Fix de magique number par des constantes + ajout de constante de taille dans fenetre
    Fix magique number par des constantes + ajout de constante de taille dans fenetre

--- src/main/java/fr/hashimiste/gui/Fenetre.java
+++ src/main/java/fr/hashimiste/gui/Fenetre.java
@@ -1,7 +1,7 @@
 package fr.hashimiste.gui;
 
 /**
- * Classe pour les paramètres de la fenêtre
+ * Classe pour les paramètres et taille de la fenêtre et à l'intérieur de la fenêtre
  * @author Arthur Dureau
  */
 public class Fenetre {
@@ -29,6 +29,38 @@ public class Fenetre {
      * Taille maximum pour la fenêtre en hauteur
      */
     public static int TAILLE_MAX_Y = 1080;
+    /**
+     * Taille minimum pour la fenêtre en largeur pour les fenêtres allongées
+     */
+    public static int TAILLE_MIN_ALLONGE_Y = 400;
+    /**
+     * Taille minimum pour la fenêtre en hauteur pour les fenêtres allongées
+     */
+    public static int TAILLE_MIN_ALLONGE_X = 950;
+    /**
+     * Taille minimum pour les composants en largeur
+     */
+    public static int TAILLE_MIN_COMPOSANT_Y = 20;
+    /**
+     * Taille minimum pour les composants en hauteur
+     */
+    public static int TAILLE_MIN_COMPOSANT_X = 30;
+    /**
+     * Taille préférée pour les composants en largeur
+     */
+    public static int TAILLE_PREF_COMPOSANT_Y = 20;
+    /**
+     * Taille préférée pour les composants en hauteur
+     */
+    public static int TAILLE_PREF_COMPOSANT_X = 30;
+    /**
+     * Taille maximum pour les composants en largeur
+     */
+    public static int TAILLE_MAX_COMPOSANT_Y = 20;
+    /**
+     * Taille maximum pour les composants en hauteur
+     */
+    public static int TAILLE_MAX_COMPOSANT_X = 80;
 
     private Fenetre() {}
 }
