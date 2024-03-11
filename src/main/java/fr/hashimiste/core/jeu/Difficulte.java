3c82609870d0b7a24bff848bc913eb524ddff7ae
Antoine <antoine@enimaloc.fr>
2024-03-11 04:09:13 +0100

    Technique du jeu
    Co-authored-by: DamiBoul <damien.boulanger2@gmail.com>

---
+++ src/main/java/fr/hashimiste/core/jeu/Difficulte.java
@@ -0,0 +1,10 @@
+package fr.hashimiste.core.jeu;
+
+/**
+ * L'énumération Difficulte définit les différents niveaux de difficulté possibles dans le jeu.
+ */
+public enum Difficulte {
+    FACILE,
+    MOYEN,
+    DIFFICILE
+}
---
+++ src/main/java/fr/hashimiste/core/jeu/Direction.java
@@ -0,0 +1,26 @@
+package fr.hashimiste.core.jeu;
+
+/**
+ * L'énumération Direction définit les différentes directions possibles dans le jeu.
+ */
+public enum Direction {
+    /**
+     * Représente le haut.
+     */
+    NORD,
+
+    /**
+     * Représente la droite.
+     */
+    EST,
+
+    /**
+     * Représente le bas.
+     */
+    SUD,
+
+    /**
+     * Représente la gauche.
+     */
+    OUEST
+}
