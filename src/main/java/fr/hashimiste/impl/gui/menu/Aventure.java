c246d9b511ec687e117e7a17ce3cb1811badb686
Antoine <antoine@enimaloc.fr>
2024-03-11 04:19:24 +0100

    Mode aventure
    Co-authored-by: ilyas <ily.abid@gmail.com>

---
+++ src/main/java/fr/hashimiste/impl/gui/menu/Aventure.java
@@ -0,0 +1,67 @@
+package fr.hashimiste.impl.gui.menu;
+
+import fr.hashimiste.core.gui.JFrameTemplateProfil;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Cette classe représente le mode Aventure du jeu.
+ * Elle hérite de JFrameTemplateProfil.
+ */
+public class Aventure extends JFrameTemplateProfil {
+
+    private final JPanel centre = new JPanel();
+    private final JPanel bas = new JPanel();
+
+    private final JButton butMenu = creerBoutton("Menu", fenetreParente);
+
+    private final JLabel labAventure = new JLabel("Mode Aventure");
+
+    // Niveau as affiché avec la classe fr.hashimiste.impl.gui.jeu.Jeu [new Jeu(this, laGrille)]
+    private final JFrame[] niveaux = new JFrame[]{
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+    };
+
+    /**
+     * Constructeur de la classe Aventure.
+     *
+     * @param parent la fenêtre parente.
+     */
+    public Aventure(JFrameTemplateProfil parent) {
+        super(parent);
+        appliquerTheme(centre, bas, labAventure);
+
+        labAventure.setAlignmentX(Component.CENTER_ALIGNMENT);
+        labAventure.setFont(new Font("Arial", Font.BOLD, 30));
+
+        centre.setLayout(new BoxLayout(centre, BoxLayout.Y_AXIS));
+        centre.add(Box.createVerticalStrut(20));
+        centre.add(labAventure);
+        centre.add(Box.createVerticalStrut(10));
+        for (int i = 0; i < niveaux.length; i++) {
+            centre.add(Box.createVerticalStrut(10));
+            JButton button = creerBoutton("Niveau " + (i + 1), niveaux[i]);
+            centre.add(button);
+            button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        }
+
+        bas.add(Box.createHorizontalGlue());
+        bas.add(butMenu);
+
+        setLayout(new BorderLayout());
+        bas.setLayout(new BoxLayout(bas, BoxLayout.X_AXIS));
+
+        add(centre, BorderLayout.CENTER);
+        add(bas, BorderLayout.SOUTH);
+    }
+}
