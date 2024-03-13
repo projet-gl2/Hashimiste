d581602ba2f6a88ea22122b1c5a5b2787c425d24
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2024-03-13 12:13:44 +0100

    Ajout de theme
    Ajout de theme : dark theme, candy theme

---
+++ src/main/java/fr/hashimiste/impl/gui/theme/CandyTheme.java
@@ -0,0 +1,78 @@
+package fr.hashimiste.impl.gui.theme;
+
+import fr.hashimiste.core.gui.Theme;
+
+import java.awt.*;
+
+/**
+ * La classe DefaultTheme implémente l'interface Theme.
+ * Elle définit un thème par défaut avec des couleurs spécifiques pour les différents éléments de l'interface utilisateur.
+ */
+public class CandyTheme implements Theme {
+    /**
+     * L'instance unique de la classe DefaultTheme.
+     */
+    public static final Theme INSTANCE = new CandyTheme();
+
+    /**
+     * La couleur des boutons dans le thème par défaut.
+     */
+    private static final Color BOUTON = new Color(255, 10, 84);
+
+    /**
+     * La couleur du texte des boutons dans le thème par défaut.
+     */
+    private static final Color TEXTE_BOUTON = new Color(250, 224, 228);
+
+    /**
+     * La couleur des boutons désactivés dans le thème par défaut.
+     */
+    private static final Color BOUTON_DESACTIVE = new Color(255, 92, 138);
+
+    /**
+     * La couleur de fond dans le thème par défaut.
+     */
+    private static final Color FOND = new Color(255, 153, 172);
+
+    /**
+     * La couleur transparente dans le thème par défaut.
+     */
+    private static final Color TRANSPARENT = new Color(0, 0, 0, 0);
+
+    /**
+     * Le constructeur privé de la classe DefaultTheme.
+     * Il est privé pour empêcher l'instanciation directe de cette classe.
+     */
+    private CandyTheme() {
+    }
+
+    @Override
+    public Color getButtonColor() {
+        return BOUTON;
+    }
+
+    @Override
+    public Color getButtonTextColor() {
+        return TEXTE_BOUTON;
+    }
+
+    @Override
+    public Color getDisabledButtonColor() {
+        return BOUTON_DESACTIVE;
+    }
+
+    @Override
+    public Color getBackgroundColor() {
+        return FOND;
+    }
+
+    @Override
+    public Color getTransparentColor() {
+        return TRANSPARENT;
+    }
+
+    @Override
+    public Color getTextColor() {
+        return Color.BLACK;
+    }
+}
---
+++ src/main/java/fr/hashimiste/impl/gui/theme/DarkTheme.java
@@ -0,0 +1,78 @@
+package fr.hashimiste.impl.gui.theme;
+
+import fr.hashimiste.core.gui.Theme;
+
+import java.awt.*;
+
+/**
+ * La classe DefaultTheme implémente l'interface Theme.
+ * Elle définit un thème par défaut avec des couleurs spécifiques pour les différents éléments de l'interface utilisateur.
+ */
+public class DarkTheme implements Theme {
+    /**
+     * L'instance unique de la classe DefaultTheme.
+     */
+    public static final Theme INSTANCE = new DarkTheme();
+
+    /**
+     * La couleur des boutons dans le thème par défaut.
+     */
+    private static final Color BOUTON = new Color(58, 1, 92);
+
+    /**
+     * La couleur du texte des boutons dans le thème par défaut.
+     */
+    private static final Color TEXTE_BOUTON = new Color(255, 255, 255);
+
+    /**
+     * La couleur des boutons désactivés dans le thème par défaut.
+     */
+    private static final Color BOUTON_DESACTIVE = new Color(79, 1, 71);
+
+    /**
+     * La couleur de fond dans le thème par défaut.
+     */
+    private static final Color FOND = new Color(56, 37, 68);
+
+    /**
+     * La couleur transparente dans le thème par défaut.
+     */
+    private static final Color TRANSPARENT = new Color(0, 0, 0, 0);
+
+    /**
+     * Le constructeur privé de la classe DefaultTheme.
+     * Il est privé pour empêcher l'instanciation directe de cette classe.
+     */
+    private DarkTheme() {
+    }
+
+    @Override
+    public Color getButtonColor() {
+        return BOUTON;
+    }
+
+    @Override
+    public Color getButtonTextColor() {
+        return TEXTE_BOUTON;
+    }
+
+    @Override
+    public Color getDisabledButtonColor() {
+        return BOUTON_DESACTIVE;
+    }
+
+    @Override
+    public Color getBackgroundColor() {
+        return FOND;
+    }
+
+    @Override
+    public Color getTransparentColor() {
+        return TRANSPARENT;
+    }
+
+    @Override
+    public Color getTextColor() {
+        return Color.WHITE;
+    }
+}
