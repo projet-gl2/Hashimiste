028108f72b12034ec3171e5b62edca02da8ab3ab
HiraAkshayy <henzoageorges@gmail.com>
2024-03-26 10:01:26 +0100

    Ajout du test du menu pour ProfilSelectionTest

--- src/test/java/fr/hashimiste/impl/gui/menu/ProfilSelectionTest.java
+++ src/test/java/fr/hashimiste/impl/gui/menu/ProfilSelectionTest.java
@@ -8,6 +8,7 @@ import fr.hashimiste.core.gui.JFrameTemplateProfil;
 import fr.hashimiste.core.joueur.Profil;
 import fr.hashimiste.impl.joueur.ProfilImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 import java.io.IOException;
@@ -26,5 +27,21 @@ class ProfilSelectionTest extends fr.hashimiste.impl.gui.menu.Test{
         return profilSelection;
     }
 
+    @Test
+    void testMenuProfilSelection(){
+        testerMenu(profilSelection, "Hashimiste", new Dimension(500, 300));
+
+        assertEquals("default", profilSelection.getProperties().getProperty("theme"), "Le thème devrait être '" + "default" + "'");
+
+        // Test d'un autre thème
+        profilSelection.getProperties().setProperty("theme", "candy");
+        assertEquals("candy", profilSelection.getProperties().getProperty("theme"), "Le thème devrait être 'candy'");
+
+        // Test de repassé au thème par défaut
+        profilSelection.getProperties().setProperty("theme", "default");
+        assertEquals("default", profilSelection.getProperties().getProperty("theme"), "Le thème devrait être '" + "default" + "'");
+
+        System.out.println("Le test du changement de thème réussi");
+    }
 
 }
